ffacfa952244af9a6b272729cbcb17dc7c5d57fe
Manjeet Yadav <99329532+ManjeetYdv@users.noreply.github.com>
2022-07-25 00:06:12 +0530

    Update addTask.java

--- TODO_List_project/src/main/java/Add/addTask.java
+++ TODO_List_project/src/main/java/Add/addTask.java
@@ -24,7 +24,7 @@ public class addTask extends HttpServlet {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todo" , "Manjeet" ,"HAha1100@");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/todo" , "Manjeet" ,"hehe");
 			String query ="insert into TaskTable(task) values(?)";
 			PreparedStatement statement = connection.prepareStatement(query);
 			statement.setString(1, taskString);
